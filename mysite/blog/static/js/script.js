24a5f856d848aedc153b66563f6b5b1b49219d6f
Aleksandr <sasha.saveylov@mail.ru>
2022-05-11 02:32:23 +0300

    for web commit

---
+++ mysite/blog/static/js/script.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    let x = document.querySelector("#myTopnav");
+    document.querySelector("#menu").onclick = function(){
+         x.classList.toggle('responsive');
+    }
+}
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
+def main(request):
+    return render(request, 'blog/main.html', {})
 # Create your views here.
