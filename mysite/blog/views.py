dc236c4cf214f90f555499fd45589b00e03b3748
DIOR777 <93180544+DIOR777@users.noreply.github.com>
2022-05-16 15:44:21 +0300

    commit from nurdin

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'blog/main.html', {})
+def location(request):
+    return render(request, 'blog/location.html', {})
 # Create your views here.
