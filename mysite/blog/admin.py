906b67ff3ddf2a976c79c781676e13bfc18249a4
Ilya <rudkach3@gmail.com>
2022-05-23 23:58:51 +0300

    commit Ilya

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -4,9 +4,11 @@ from .models import Post
 from .models import MyHokku
 from .models import Tokio_authors
 from .models import Project
+from .models import NewResponse
 
 admin.site.register(Post)
 admin.site.register(MyHokku)
 admin.site.register(Tokio_authors)
 admin.site.register(Project)
+admin.site.register(NewResponse)
 # Register your models here.
